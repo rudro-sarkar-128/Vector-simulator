5e18c89a3ea9789b36eb2c039b55320bd3c8d818
rudro-sarkar-128 <rudrosarkar128@gmail.com>
2025-06-05 14:39:47 +0600

    Direct Upload

---
+++ src/scripts/vector.js
@@ -0,0 +1,13 @@
+
+const vectors = {
+    vector_1 : {},
+    vector_2 : {},
+    vector_3 : {},
+    vector_4 : {},
+    vector_5 : {},
+    vector_6 : {},
+    vector_7 : {},
+    vector_8 : {},
+    vector_9 : {},
+    vector_10 : {}
+}
